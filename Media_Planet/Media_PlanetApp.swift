9a1d962a827866904e0ff33a0349f4ddddad0012
SERGIO ACOSTA <42353861+sergio2000@users.noreply.github.com>
2023-01-17 21:39:00 -0800

    Initial Commit

---
+++ Media_Planet/Media_PlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Media_PlanetApp.swift
+//  Media_Planet
+//
+//  Created by Sergio Acosta on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Media_PlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
